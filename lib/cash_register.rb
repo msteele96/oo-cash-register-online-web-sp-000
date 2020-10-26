182a356f08001366a624189f5336da7cb0673fbc
Marc Steele <marcsteele2014@gmail.com>
2020-10-26 16:42:52 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,16 +1,18 @@
 class CashRegister
 
-  attr_accessor :discount, :total
+  attr_accessor :discount, :total, :last_transaction
 
   def initialize(discount = 0)
     @total = 0
     @discount = discount.to_f
     @cart = []
+    @last_transaction = []
   end
 
   def add_item(item, price, quantity = 1)
     @total += price * quantity
     quantity.times {@cart << item}
+    @last_transaction = [item, price, quantity]
   end
 
   def apply_discount
