04bf2bdc59a2f473d9b51c7465b38c9674247606
Marc Steele <marcsteele2014@gmail.com>
2020-10-26 16:37:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
 
   def add_item(item, price, quantity = 1)
     @total += price * quantity
-    @cart << item * quantity
+    @cart << (item * quantity).split
   end
 
   def apply_discount
